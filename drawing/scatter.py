cc269bb88015df0f0e17bd638ac47ed16b01da49
ZihaoZhouSCUT <eezihaozhou@gmail.com>
2024-05-21 21:28:01 +0800

    add drawing module

---
+++ drawing/scatter.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from utils import config
+from utils.util_function import euclidean_distance
+from phy.large_scale_fading import maximum_communication_range
+
+
+# draw a static scatter plot, includes communication edges
+def scatter_plot(simulator):
+    fig = plt.figure()
+    ax = fig.add_axes(Axes3D(fig))
+
+    for drone1 in simulator.drones:
+        for drone2 in simulator.drones:
+            if drone1.identifier != drone2.identifier:
+                ax.scatter(drone1.coords[0], drone1.coords[1], drone1.coords[2], c='red', s=30)
+                distance = euclidean_distance(drone1.coords, drone2.coords)
+                if distance <= maximum_communication_range():
+                    x = [drone1.coords[0], drone2.coords[0]]
+                    y = [drone1.coords[1], drone2.coords[1]]
+                    z = [drone1.coords[2], drone2.coords[2]]
+                    ax.plot(x, y, z, color='black', linestyle='dashed', linewidth=1)
+
+    ax.set_xlim(0, config.MAP_LENGTH)
+    ax.set_ylim(0, config.MAP_WIDTH)
+    ax.set_zlim(0, config.MAP_HEIGHT)
+    plt.show()
