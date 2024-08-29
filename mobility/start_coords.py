5012c62a6eda816aff3bc4cfa9cff829984e9faf
ZihaoZhouSCUT <eezihaozhou@gmail.com>
2024-08-29 10:41:53 +0800

    update: deploy in 3-D cube space

--- mobility/start_coords.py
+++ mobility/start_coords.py
@@ -8,7 +8,7 @@ def get_random_start_point_3d(sim_seed):
         random.seed(sim_seed + i)
         position_x = random.uniform(50, config.MAP_LENGTH - 50)
         position_y = random.uniform(50, config.MAP_WIDTH - 50)
-        position_z = random.uniform(10, config.MAP_HEIGHT - 10)
+        position_z = random.uniform(50, config.MAP_HEIGHT - 50)
 
         start_position.append(tuple([position_x, position_y, position_z]))
 
